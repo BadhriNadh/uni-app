4e41daf7e120f394ff12a825ed6724a8136d7bc2
Badhri Nadh Arja <arja.badhrinadh@gmail.com>
2023-11-26 16:47:23 -0400

    Google maps and share

---
+++ app/src/main/java/com/example/roomdb/Fragments/MapFragments/TransitFragment.kt
@@ -0,0 +1,27 @@
+package com.example.roomdb.Fragments.MapFragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomdb.R
+
+
+class TransitFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_transit, container, false)
+
+        return view
+    }
+}
